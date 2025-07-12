409deb8a4cc8b06529f527a653816f812c0fe753
ARUN-apu <arunsenapati407@gmail.com>
2025-07-13 00:25:34 +0530

    1st

---
+++ 1st.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int favourite_no;
+
+    std::cout<<"Enter your Favourite no in between 10 to 1oo: ";
+    std::cin>>favourite_no;
+    std::cout<<"Amazing "<<favourite_no<<" is my favourite no too!";
+    return 0;
+
+}
